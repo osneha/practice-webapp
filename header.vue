7d87d8681f10a886c3bb10c7e7e80799307a3320
Sneha Ojha <42754345+osneha@users.noreply.github.com>
2021-02-08 10:31:40 -0600

    changes

---
+++ header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <h3>{{ title }} </h3>
+    </header>
+</template>
+
+<script>
+export default {
+    data(){
+       return{ 
+       title : "NORTHWEST MISSOURI STATE UNIVERSITY"
+       }
+    }
+    
+}
+</script>
+<style scoped>
+header{
+    background-color: lightgreen;
+    padding : 15px;
+    height : 70px;
+    text-align: center;
+    color: darkgreen;
+}
+</style>
+
